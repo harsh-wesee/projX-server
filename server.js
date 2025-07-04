02227f514af80b06f553717ac535b4997be463e9
HarshTripathi <tripathiharsh16@gmail.com>
2025-07-04 21:01:15 +0530

    port changed

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ const waitlistRoutes = require('./routes/soft-launch-routes/waitlist')
 
 
 const app = express();
-const PORT = process.env.PORT || 5001;
+const PORT = process.env.PORT || 5000;
 
 
 app.use(cors());
